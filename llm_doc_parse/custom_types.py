9339d1f6da3f0ee13118b503a5495229f0fc42d9
inwonakng <inwon.kang04@gmail.com>
2023-11-12 01:59:05 -0500

    new way to try prompts

--- llm_doc_parse/custom_types.py
+++ llm_doc_parse/custom_types.py
@@ -1,7 +1,3 @@
 from typing import Literal
 
 OpenAIMessage = list[dict[str, str]]
-TextGenMessage = dict[
-    Literal['user_input','history','context_instruct'], 
-    str | dict[str, list[list[str]]]
-]
--- llm_doc_parse/prompt.py
+++ llm_doc_parse/prompt.py
@@ -1,69 +1,99 @@
-from typing import Literal
+from typing import Self
 import yaml
+from dataclasses import dataclass, field
 
-from .config.path import TEMPLATE_PATH
+from .custom_types import OpenAIMessage
+from .api import generate
 
-def load_template(
-    mode: Literal['use_original', 'use_previous', 'continue'],
-    logic: Literal['n3', 'cql'],
-) -> list[dict[str,str|list[dict[str,str]]]]:
-    if mode == 'use_original':
-        if logic == 'n3':
-            filename = TEMPLATE_PATH / 'use_original_n3.yaml'
-        elif logic == 'cql':
-            filename = TEMPLATE_PATH / 'use_original_cql.yaml'
-        else:
-            raise Exception(f'Unknown logic [{logic}]')
-    elif mode == 'use_previous':
-        if logic == 'n3':
-            filename = TEMPLATE_PATH / 'use_previous_n3.yaml'
-        elif logic == 'cql':
-            filename = TEMPLATE_PATH / 'use_previous_cql.yaml'
-        else:
-            raise Exception(f'Unknown logic [{logic}]')
-    elif mode == 'continue':
-        if logic == 'n3':
-            filename = TEMPLATE_PATH / 'continue_n3.yaml'
-        elif logic == 'cql':
-            filename = TEMPLATE_PATH / 'continue_cql.yaml'
-        else:
-            raise Exception(f'Unknown logic [{logic}]')
-    else:
-        raise Exception(f'Unknown mode [{mode}]')
-    template = yaml.safe_load(open(filename))
-    messages_per_task = [
-        {
-            'task_name': task,
-            'messages': [
-                {'role': 'system', 'content': task_prompt['system']},
-                {'role': 'user', 'content': task_prompt['user']},
-            ]
+VOWELS = 'aeiou'
+
+@dataclass
+class Task:
+    name: str
+    output_name: str
+    messages: dict[str,str]
+
+@dataclass
+class ExecutableInput:
+    name: str
+    components: list[str] = field(default_factory=list) 
+
+@dataclass
+class Executable:
+    task: str
+    messages: list[str]
+    input: ExecutableInput
+    prefix: str | None = None
+    
+    def __post_init__(self):
+        self.input = ExecutableInput(**self.input)
+
+def replace_input_name(text, input_name):
+    an_input_name = f'an {input_name}' if input_name[0].lower() in VOWELS else f'a {input_name}'
+    text = text.replace(
+        '<<AN_INPUT_NAME>>', an_input_name
+    ).replace(
+        '<<THE_INPUT_NAME>>', f'the {input_name}'
+    )
+    return text
+    
+class Prompt:
+    tasks: dict[str,Task]
+    continue_message: str
+    responses: dict[str,str]
+
+    def __init__(self, tasks: list[Task], continue_message: str) -> None:
+        self.tasks = {
+            task.name: task
+            for task in tasks
         }
-        for task, task_prompt in template.items()
-    ]
-    return messages_per_task
+        self.continue_message = continue_message
+
+    def execute(
+        self, 
+        model: str,
+        configs: list[dict[str,str|list[str]]], 
+        document: str
+    ) -> None:
+        valid_input_components = dict(document=document)
+        for raw_conf in configs:
+            executable = Executable(**raw_conf)
+            task = self.tasks[executable.task]
+            system_msg = '\n'.join(
+                replace_input_name(
+                    task.messages[message],
+                    executable.input.name,
+                )
+                for message in executable.messages
+            )
+            user_msg = '\n'.join(
+                f'### {input_component.capitalize()}\n' + 
+                valid_input_components[input_component]
+                for input_component in executable.input.components
+            )
+            messages = [
+                {'role': 'system', 'content': system_msg,},
+                {'role': 'user', 'content': user_msg,}
+            ]
+            
+            output = generate(
+                model, 
+                messages
+            )
+            valid_input_components[executable.task] = output
+            
+            print('-'*20+'system'+'-'*20)
+            print(system_msg)
+            print()
+            print('-'*20+'user'+'-'*20)
+            print(user_msg)
+            print('-'*20+'assistant'+'-'*20)
+            print(output)
+            print('='*80)
 
 
-def fill_template(
-    template: str,
-    original_doc: str,
-    responses: dict[str,str] | None = None,
-):
-    filled_content = template['content']
-    if '{ORIGINAL_DOC}' in filled_content:
-        filled_content = filled_content.replace(
-            '{ORIGINAL_DOC}', original_doc
-        )
-    if '{TASK_1_RESPONSE}' in filled_content and responses:
-        filled_content = filled_content.replace(
-            '{TASK_1_RESPONSE}', responses['task_1']
-        )
-    if '{TASK_2_RESPONSE}' in filled_content and responses:
-        filled_content = filled_content.replace(
-            '{TASK_2_RESPONSE}', responses['task_2']
-        )
-
-    return {
-        **template,
-        'content': filled_content
-    }
+    @staticmethod
+    def load_from_files(tasks_file, cont_file) -> 'Prompt':
+        continue_message = open(cont_file).read()
+        tasks = [Task(**task) for task in yaml.safe_load(open(tasks_file))]
+        return Prompt(tasks, continue_message)
