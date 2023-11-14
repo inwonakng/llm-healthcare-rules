d25361b90c5b8d2c5408f2fde4b01a5387b65d32
inwonakng <inwon.kang04@gmail.com>
2023-11-14 01:55:58 -0500

    removing cleaner code

--- llm_doc_parse/eval_unit_test.py
+++ llm_doc_parse/eval_unit_test.py
@@ -48,7 +48,6 @@ def run(
             # prepare directories for saving 
             report_dir = REPORT_DIR/model/doc_name/mode
             report_dir.mkdir(parents=True, exist_ok=True)
-            shutil.rmtree(save_dir / 'code')
             code_save_dir = report_dir / 'code'
             code_save_dir.mkdir(parents=True, exist_ok=True)
             
